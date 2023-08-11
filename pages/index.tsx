0fccb158bfbd4ae9676884237b1d52deafec531f
杨万青 <yangwanqing.me@qq.com>
2023-08-11 15:01:23 +0800

    Fix index page fetch tree data multi times

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,52 +3,47 @@ import { useEffect, useState } from "react";
 import Router, { useRouter } from "next/router";
 
 export default function Index(props) {
-  const [firstKey, setFirstKey] = useState("")
-
   if (typeof window !== "undefined") {
     const router = useRouter();
-    const { asPath } = router;
-    const url = asPath === "/" ? '/api/tree' : '/api/tree?isPreview=true'
+    const { query } = router;
+    const { isPreview } = query;
+    if (isPreview !== undefined) {
+      const url = isPreview ? '/api/tree?isPreview=true' : '/api/tree'
 
-    const findFirstFileKey = (node) => {
-      if (node && node.children && node.children.length > 0) {
-        for (const child of node.children) {
-          if (child.type === "file") {
-            return child.key
-          } else if (child.type === "folder") {
-            const firstFileKey = findFirstFileKey(child)
-            if (firstFileKey) {
-              return firstFileKey
-            }
+      console.log('router: ', router)
+      fetch(url).then((response) => {
+        response.json().then(({ result }) => {
+          console.log('fetch fullTreeData>>', result);
+          const firstProject = result[0]
+          const firstVersion = firstProject.children[0]
+          const firstLanguage = firstVersion && firstVersion.children[0]
+          const firstPlatform = firstLanguage && firstLanguage.children[0]
+          const firstFileKey = firstPlatform && findFirstFileKey(firstPlatform)
+          console.log('firstFileKey', firstFileKey);
+          if (firstFileKey) {
+            Router.push("docs/" + firstFileKey);
           }
-        }
-
-      }
-      return undefined;
-    }
-
-    fetch(url).then((response) => {
-      response.json().then(({ result }) => {
-        console.log('fetch fullTreeData', result);
-        const firstProject = result[0]
-        const firstVersion = firstProject.children[0]
-        const firstLanguage = firstVersion && firstVersion.children[0]
-        const firstPlatform = firstLanguage && firstLanguage.children[0]
-        const firstFileKey = firstPlatform && findFirstFileKey(firstPlatform)
-        console.log('firstFileKey', firstFileKey);
-        if (firstFileKey) {
-          setFirstKey(firstFileKey)
-        }
+        });
       });
-    });
+    }
   }
 
-  useEffect(() => {
-    if (firstKey) {
-      Router.push("docs/" + firstKey);
+  const findFirstFileKey = (node) => {
+    if (node && node.children && node.children.length > 0) {
+      for (const child of node.children) {
+        if (child.type === "file") {
+          return child.key
+        } else if (child.type === "folder") {
+          const firstFileKey = findFirstFileKey(child)
+          if (firstFileKey) {
+            return firstFileKey
+          }
+        }
+      }
 
     }
-  }, [firstKey])
+    return undefined;
+  }
 
   return (
     <div />
